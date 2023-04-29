e64eb33c15822f369ad748c3f066cc3153fb681e
Bupe Mulenga <creativedirectorctl2018@gmail.com>
2023-04-29 03:58:11 +0200

    Add User Interface class and define methods

--- src/main/java/com/sudoku/javadesktopsudoku/SudokuApplication.java
+++ src/main/java/com/sudoku/javadesktopsudoku/SudokuApplication.java
@@ -1,8 +1,8 @@
 package com.sudoku.javadesktopsudoku;
 
+import com.sudoku.javadesktopsudoku.interfaces.IUserInterfaceContract;
+import com.sudoku.javadesktopsudoku.interfaces.UserInterfaceImpl;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
